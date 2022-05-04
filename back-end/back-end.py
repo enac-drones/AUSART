de69c36bd1f9d05fdf21c78e9b2177117ab10a30
Corentin.Conan <corentin.conan@enac.fr>
2022-05-04 14:20:49 +0200

    auth and refresh UCIS auth token

--- back-end/back-end.py
+++ back-end/back-end.py
@@ -1,29 +1,116 @@
 #!/usr/bin/python3
 
+import time
 
 from ivy.std_api import *
-from enum import Enum
+
+import requests
 import json
+
+import threading
 
 from shapely.geometry import Point
 from shapely.geometry import shape
 from shapely.geometry.polygon import Polygon
 
 
-
 class BackEnd():
+
 
 	def __init__(self):
 
-		IvyInit("AUSART_BACK_END")
-		IvyStart()
+		self.client_id = 'd7d08988-97ba-44af-a7e1-afab0524510b'
+		self.username = 'cconan'
+		self.password = 'wac_2022'
+		self.token = None
+		self.refresh_token = None
+		self.headers = None
 
 		self.sectors = []
 
-		## Ivy bindings to communicate with front end
+		IvyInit("AUSART_BACK_END")
+		IvyStart()
 		IvyBindMsg(self.send_sectors, "FRONT_END_READY")
 
+		self.log_in_to_UCIS()
+
+
+
+
+	def log_in_to_UCIS(self):
 		## Log in as ANSP into UCIS system ##
+		url = 'https://www.ucis.ssghosting.net/auth/realms/UCIS/protocol/openid-connect/token'
+
+		headers = {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		}
+
+		data = {
+			'client_id': self.client_id,
+			'username': self.username,
+			'password': self.password,
+			'grant_type': 'password',
+			'scope': 'flight-planning.dops.read flight-planning.dops.write ' + \
+			'flight-planning.dops.activate.write flight-planning.dops.approve.write ' + \
+			'flight-planning.dops.cancel.write flight-planning.dops.close.write ' + \
+			'flight-planning.dops.reject.write flight-planning.dops.submit.write ' + \
+			'geoawareness.uaszones.read geoawareness.uaszones.write ' + \
+			'geoawareness.uaszones.submit.write'
+		}
+
+		response = requests.post(url, headers=headers, data=data)
+		print("\nAuthentifying")
+
+		if response.status_code == 200:
+			print("\nAuthentified successfully")
+			print(response.text)
+			self.token = response.json()["access_token"]
+			self.refresh_token = response.json()["refresh_token"]
+
+			self.headers = {
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + self.token
+			}
+
+			expires_in = response.json()["expires_in"]
+
+			refresh_thread = threading.Thread(target=self.thread_refresh,
+				args=(expires_in,), daemon=False)
+			refresh_thread.start()
+
+
+		else:
+			print("\nLog in failed:", response.text)
+
+
+
+
+	def thread_refresh(self, expires_in):
+		## Creates a new thread to auto refresh the access token ##
+		while True:
+			#print("\nRefresh thread started")
+			time.sleep(expires_in - 50)
+			#print("\nAutomatic token refresh")
+
+			url = 'https://www.ucis.ssghosting.net/auth/realms/UCIS/protocol/openid-connect/token'
+
+			data = {
+				'grant_type': 'refresh_token',
+				'client_id': self.client_id,
+				'refresh_token': self.refresh_token
+			}
+
+			response = requests.post(url, data=data)
+
+			if response.status_code == 200:
+				print("\nAutomatic token refresh success")
+				#print(response.text)
+				self.refresh_token = response.json()["refresh_token"]
+				self.token = response.json()["refresh_token"]
+
+			else:
+				print("\nFailed to refresh token")
+
 
 
 
