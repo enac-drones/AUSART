479378fcececb490fd17b4a84ee45f510ffe6cb8
Corentin.Conan <corentin.conan@enac.fr>
2022-05-10 11:11:14 +0200

    flight plan manages empty or missing fields / flight plan attribute geometries

--- back-end/flight_plan.py
+++ back-end/flight_plan.py
@@ -1,3 +1,7 @@
+from geometry import Geometry
+
+
+
 class FlightPlan():
 
 
@@ -6,16 +10,47 @@ class FlightPlan():
 		self.uuid = json["uuid"]
 		self.expected_start = json["expected_start"]
 		self.expected_end = json["expected_end"]
-		self.effective_start = json["effective_start"]
-		self.effective_end = json["effective_end"]
-		self.drone = json["drone"]
-		self.swarm = json["swarm"]
-		self.pilots = json["pilots"]
-		self.operation_type = json["operation_type"]
-		self.operaton_domain = json["operation_domain"]
-		self.additional_info_for_atc = json["additional_info_for_atc"]
+		try:
+			self.effective_start = json["effective_start"]
+		except KeyError:
+			self.effective_start = None
+		try:
+			self.effective_end = json["effective_end"]
+		except KeyError:
+			self.effective_end = None
+		try:
+			self.drone = json["drone"]
+		except KeyError:
+			self.drone = None
+		try:
+			self.swarm = json["swarm"]
+		except KeyError:
+			self.swarm = None
+		try:
+			self.pilots = json["pilots"]
+		except KeyError:
+			self.operation_type = None
+		try:
+			self.operaton_domain = json["operation_domain"]
+		except KeyError:
+			self.operaton_domain = None
+		try:
+			self.additional_info_for_atc = json["additional_info_for_atc"]
+		except KeyError:
+			self.additional_info_for_atc = None
 		self.geospatial_occupancy = json["geospatial_occupancy"]
 		self.status = json["status"]
 		self.status_detail = json["status_detail"]
-		self.contingency_plan = json["contingency_plan"]
+		try:
+			self.contingency_plan = json["contingency_plan"]
+		except KeyError:
+			self.contingency_plan = None
 		self.metadata = json["metadata"]
+
+		self.geometries = []
+
+		section_id = 0
+		for section in self.geospatial_occupancy:
+			geometry = Geometry(section_id, section)
+			self.geometries.append(geometry)
+			section_id += 1
---
+++ back-end/geometry.py
@@ -0,0 +1,39 @@
+class Geometry():
+
+
+	def __init__(self, _id, json):
+
+		self.id = _id
+		self.type = ""
+		try:
+			altitude_lower = json["volume"]["altitude_lower"]
+			if altitude_lower is not None:
+				self.altitude_lower = altitude_lower["value"]
+		except KeyError:
+			self.altitude_lower = 0
+		self.altitude_upper = json["volume"]["altitude_upper"]["value"]
+		# if circle #
+		self.center_lon = None
+		self.center_lat = None
+		self.radius = None
+		# if polygon #
+		self.coords = []
+
+		if json["geospatialOccupancyType"] == "Volume4D":
+			if json["volume"]["outline_circle"] is not None:
+				print("CREATING A CIRCLE GEOMETRY")
+				self.type = "circle"
+				self.center_lon = json["volume"]["outline_circle"]["geometry"]["coordinates"][0]
+				self.center_lat = json["volume"]["outline_circle"]["geometry"]["coordinates"][1]
+				self.radius = json["volume"]["outline_circle"]["properties"]["radius"]["value"]
+			elif json["outline_polygon"] is not None:
+				print("CREATING A POLYGON GEOMETRY")
+				if json["volume"]["outline_polygon"]["type"] == "Polygon":
+					for coords in json["outline_polygon"]["coordinates"]:
+						self.coords.append([coords[0], coords[1]])
+				else:
+					print("UNSUPPORTED POLYGON TYPE")
+		elif json["geospatialOccupancyType"] == "Trajectory4D":
+			print("DOES NOT SUPPORT TRAJECTORY 4D TYPE YET")
+		else:
+			print("UNSUPPORTED GEOMETRY TYPE FOR GEOMETRY ID : ", self.id)
